1d6769ce76a42bc431c9a50b89adc43a70eaf9b2
jagadru <jagadrub@gmail.com>
2021-08-02 21:25:56 -0300

    Update requeriments dependencies

--- run.py
+++ run.py
@@ -45,9 +45,9 @@ class Command:
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--phone_number', type=str, required=True)
+    parser.add_argument('--phone-number', type=str, required=True)
     parser.add_argument('--file', type=str, required=True)
-    parser.add_argument('--billing_period', type=str, required=True)
+    parser.add_argument('--billing-period', type=str, required=True)
 
     args = parser.parse_args()
     Command(args.phone_number, args.file, args.billing_period).run()
